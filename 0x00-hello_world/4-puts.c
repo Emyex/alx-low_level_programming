24b8800a50310778359050a80d932d30900282bb
Emyex <shinaalonge772@gmail.com>
2022-09-08 16:44:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <studio.h>
 /**
- * main -entry block
+ * main - entry block
  * return : o after printing the function
  */
 int main(void)
-{puts ( "\" programming is like building a multilingual puzzlee" );
+{puts ("\" programming is like building a multilingual puzzlee");
 	return (0);
 }
