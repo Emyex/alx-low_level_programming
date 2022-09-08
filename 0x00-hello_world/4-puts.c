a2999372cd1ba6ebf83e7b310d1362b540146112
Emyex <shinaalonge772@gmail.com>
2022-09-08 16:55:30 -0700

    ghp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <studio.h>
 /**
  * main - entry block
- * return: 0 after printing the function
+ * return: always 0
  */
 int main(void)
 {puts ("\" programming is like building a multilingual puzzlee");
