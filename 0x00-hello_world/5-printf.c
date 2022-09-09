cdac925c54010351c6fc113a3cdf7f1a6026b62e
Emyex <shinaalonge772@gmail.com>
2022-09-08 18:25:07 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * Return: always 0 preview
 */
 int main(void)
-{	printf("with proper grammar, but the outcome is a piece of art, \n");
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
