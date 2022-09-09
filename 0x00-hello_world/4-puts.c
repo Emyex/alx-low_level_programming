98244f35d4bf28b55e57633e247c38886549d35b
Emyex <shinaalonge772@gmail.com>
2022-09-08 17:38:04 -0700

    so good

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry block
- * return: 0 always
+ * return: always 0
  */
 int main(void)
-{puts ("\"programming is like building a multilingual puzzle");
+{
+	puts ("\"programming is like building a multilingual puzzle");
 	return (0);
 }
