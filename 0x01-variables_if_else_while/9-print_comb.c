cd51161110b0ba3dbe3c089fa4ec444a08ea2165
Emyex <shinaalonge772@gmail.com>
2022-09-12 14:50:45 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible combinations
+ * of single-digit numbers.
+ * Return: always 0
+ */
+int main(void)
+
+{
+
+int i;
+
+for (i = 10; i < 20; i++)
+{
+putchar((i % 10) + '0');
+
+if (i != 19)
+
+{
+
+putchar(',');
+
+putchar(' ');
+
+}
+}
+
+putchar('\n');
+
+return (0);
+}
+
