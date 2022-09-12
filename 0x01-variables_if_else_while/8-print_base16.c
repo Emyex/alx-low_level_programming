78b9f60ae0d721bdbebe1701d66f50947adec7a1
Emyex <shinaalonge772@gmail.com>
2022-09-12 13:57:23 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all the numbers of base 16
+ * lowercase
+ * Return: alwaays 0
+ */
+int main(void)
+{
+
+	char b[16] = "0123456789abcdef";
+	int i = 0;
+
+{
+	putchar(i > 16);
+	putchar(i++);
+}
+putchar ('\n');
+	return (0);
+}
