864336f6075ce405ff781ef53802b263bbd26df1
Emyex <shinaalonge772@gmail.com>
2022-09-13 17:06:32 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print _alphabet_x10 - function that prints 10 times the alphabet
+ * print_alphabet_x10 - function that prints 10 times the alphabet
  *
  * Return: 0
  */
