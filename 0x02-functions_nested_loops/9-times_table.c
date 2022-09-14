0a20566aba757378b16bd32baf8ae84d87649084
Emyex <shinaalonge772@gmail.com>
2022-09-14 11:36:22 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - times table function
+ *
+ */
+void times_table(void)
+{
+	int i;
+
+	int j;
+
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			k = j * i;
+
+if (k <= 9)
+			{	_putchar(' ');
+			}
+else
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+_putchar('\n');
+}
+}
+
