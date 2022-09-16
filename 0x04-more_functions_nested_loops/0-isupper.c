19a33b32a89e51893d298945aab87a62779ccd4b
Emyex <shinaalonge772@gmail.com>
2022-09-16 12:11:14 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,16 @@
 /**
  * _isupper - function that checks for uppercase character.
  * @c: character being tested
- * Return: Always 0
+ * Return: 1 whether it is, 0 otherwise
  */
+
 int _isupper(int c)
 
 {
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
