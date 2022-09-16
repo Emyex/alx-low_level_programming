4ec79aa9a318b8081c5d4678ad0696113754c664
Emyex <shinaalonge772@gmail.com>
2022-09-16 16:51:35 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *@n: number of times to print the line
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
