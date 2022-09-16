f23ee6b8f01bdd7e6b2988f40f16110c05ba9847
Emyex <shinaalonge772@gmail.com>
2022-09-16 16:40:06 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,16 @@
 void more_numbers(void)
 {
 
-	int x;
-
-int y;
+	int x, y;
 
 	for (x = 0; x <= 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
 			if (y > 9)
+			{
 				_putchar((y / 10) + '0');
+			}
 			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
