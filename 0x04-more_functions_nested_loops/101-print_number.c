834792ae55fdeefdc7f809d687654d251ba2063b
Emyex <shinaalonge772@gmail.com>
2022-09-16 18:43:56 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_number - print a number
+ * @n: the number to print
+ */
+
+void print_number(int n)
+{
+unsigned int num = n;
+if (n < 0)
+{
+putchar('-');
+num = -num;
+}
+if (num > 9)
+{
+print_number(num / 10);
+}
+putchar(num % 10 + '0');
+}
