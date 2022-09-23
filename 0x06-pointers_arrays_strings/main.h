c479f9b26a78bab54627c03d7af50061ec21191d
Emyex <shinaalonge772@gmail.com>
2022-09-22 17:00:58 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prptotypes()
+ */
 char *_strcat(char *dest, char *src)
 char *_strncat(char *dest, char *src, int n)
 char *_strncpy(char *dest, char *src, int n)
