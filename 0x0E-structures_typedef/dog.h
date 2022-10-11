48806f33c3ef2bd707cf14cb4dac909824cde435
Emyex <shinaalonge772@gmail.com>
2022-10-11 10:24:24 -0700

    i can do this through christ that strengths me

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * struct dog - dog to classify
@@ -12,10 +12,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d)
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
