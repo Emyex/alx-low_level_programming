b8dfe404ab5aa86c9033f2982d66953ec2fb7a48
Emyex <shinaalonge772@gmail.com>
2022-11-20 22:56:31 -0800

    doings

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name_as_is - prints a name as is
  * @name: name of the person
- *
+ * @f: pointer to a funtion
  * Return: Nothing.
  */
 
