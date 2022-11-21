923abacac2d06f37ebc2990b4aac6e276bd1a392
Emyex <shinaalonge772@gmail.com>
2022-11-20 22:46:31 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+
+void print_name_as_is(char *name,  void (*f)(char *))
+{
+
+	if (name != NULL && f != NULL)
+
+		f(name);
+
+}
