ff625e50bcb0a0e13b2d61924ae914dfbdfb0ae1
Emyex <shinaalonge772@gmail.com>
2022-12-08 15:04:11 -0800

    doing C

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,6 @@
-#ifndef LISTS_H_
-#define LISTS_H_
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -10,6 +11,7 @@ int n;
 struct dlistint_s *prev;
 struct dlistint_s *next;
 } dlistint_t;
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -20,4 +22,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-endif /*LISTS_H_*/
+endif /**_LISTS_H_**/
